a921fa57a24850309042373a4bf4f2d092bf2051
nattawutkp <nattawut.kp@gmail.com>
2021-03-13 09:19:07 +0700

    add company_data

--- register/login.php
+++ register/login.php
@@ -7,7 +7,7 @@ $user = $_POST['user'];
 $pass = $_POST['pass'];
 
 
-$sql1 = "select staffid,pname,fname,lname,user,pass,mode,branchid,company_code from tb_staff where user='$user' and pass='$pass' ";
+$sql1 = "select staffid,pname,fname,lname,user,pass,mode,branchid,company_code,company_data from tb_staff where user='$user' and pass='$pass' ";
 $result = mysql_query($sql1) or die ("Error Query ".$sql1); 
 $n = mysql_num_rows($result);
 
@@ -19,6 +19,7 @@ if(!empty($n)){
 	$_SESSION["SYS_ENAME"] = $rs['pname'].$rs['fname'].'    '.$rs['lname']; 
 	$_SESSION["mode"] = $rs['mode']; 
 	$_SESSION["branch_id"] = $rs['branchid']; 
+	$_SESSION["company_data"] = $rs['company_data']; 
 	$_SESSION["company_code"] = $rs['company_code']; 
 	$SYS_EID= $_SESSION["SYS_EID"] ;
 	$SYS_ENAME= $_SESSION["SYS_ENAME"] ;
